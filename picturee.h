90496567d78e8c3ff581ae6fcd6ebda8e4561d3f
Merediii <realmadridivy@126.com>
2018-09-05 01:52:44 +0800

    新增注释

--- picturee.h
+++ picturee.h
@@ -4,26 +4,30 @@
 #ifndef DATA_STRUCTURE_PICTURE_H
 #define DATA_STRUCTURE_PICTURE_H
 #define maxSize 100
+//顶点定义
 typedef struct  VertexType{
     int no;
     char info;
 }VertexType;
+//
 typedef struct MGraph{
     int edge[maxSize][maxSize];
     int n,e;
     VertexType vex[maxSize];
 }MGraph;
+
+
 typedef struct ArcNode{
     int adjvex;
     struct ArcNode *next;
-    int info;
+    int info;//权值
 }ArcNode;
 typedef struct VNode{
     char data;
     ArcNode *first;
 }VNode;
 typedef struct AGraph{
-    VNode list[maxSize];
+    VNode list[maxSize];//邻接表
     int n,e;
 }AGraph;
 
