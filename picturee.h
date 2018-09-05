3866a80ee195a9123c239d38904a290048d3d208
Merediii <realmadridivy@126.com>
2018-09-05 19:49:17 +0800

    新增Prim算法

--- picturee.h
+++ picturee.h
@@ -3,7 +3,9 @@
 //
 #ifndef DATA_STRUCTURE_PICTURE_H
 #define DATA_STRUCTURE_PICTURE_H
-#define maxSize 100
+#include <algorithm>
+using namespace std;
+#define INF 0x3f3f3f3f
 //顶点定义
 typedef struct  VertexType{
     int no;
@@ -30,5 +32,42 @@ typedef struct AGraph{
     VNode list[maxSize];//邻接表
     int n,e;
 }AGraph;
+
+const int V = 7;
+int cost[V][V]={INF,1,INF,INF,INF,INF,INF,
+                1,INF,2,3,INF,7,INF,
+                INF,2,INF,INF,10,INF,INF,
+                INF,3,INF,INF,INF,1,5,
+                INF,INF,10,INF,INF,5,INF,
+                INF,1,INF,1,INF,INF,8,
+                INF,INF,INF,5,INF,8,INF};
+int mincost[V];
+bool used[V];
+
+int prim(){
+    for (int i = 0; i < V; ++i) {
+        mincost[i] = INF;
+        used[i] = false;
+    }
+
+    mincost[0] = 0;
+    int res = 0;
+
+    while (true) {
+        int v = -1;
+        for (int i = 0; i < V; ++i)
+            if (!used[i] && (v == -1 || mincost[i] < mincost[v]))
+                v = i;
+        if (v == -1) break;
+        used[v] = true;
+        cout << v  << endl;
+        res += mincost[v];
+        for (int j = 0; j < V; ++j) {
+            mincost[j] = min(mincost[j], cost[j][v]);
+        }
+        Show(mincost, 7);
+    }
+    return res;
+}
 
 #endif //DATA_STRUCTURE_PICTUREE_H
